f875b54596ac6fefcce9b04d852a24d46dd27384
hoangnhung <thuyle1181978@gmail.com>
2024-04-11 17:13:31 +0700

    update

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -115,20 +115,52 @@ const getAllOrderDetails = (id) => {
 const cancelOrderDetails = (id, data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const order = await Order.findByIdAndDelete(id)
-            if (order === null) {
+            let order = []
+            const promises = data.map(async (order) => {
+                const productData = await Product.findOneAndUpdate(
+                    {
+                        _id: order.product,
+                        selled: { $gte: order.amount }
+                    },
+                    {
+                        $inc: {
+                            countInStock: +order.amount,
+                            selled: -order.amount
+                        }
+                    },
+                    { new: true }
+                )
+                if (productData) {
+                    order = await Order.findByIdAndDelete(id)
+                    if (order === null) {
+                        resolve({
+                            status: 'ERR',
+                            message: 'The order is not defined'
+                        })
+                    }
+                } else {
+                    return {
+                        status: 'OK',
+                        message: 'ERR',
+                        id: order.product
+                    }
+                }
+            })
+            const results = await Promise.all(promises)
+            const newData = results && results[0] && results[0].id
+
+            if (newData) {
                 resolve({
                     status: 'ERR',
-                    message: 'Không tồn tại'
+                    message: `San pham voi id: ${newData} khong ton tai`
                 })
             }
             resolve({
                 status: 'OK',
-                message: 'Thành công',
+                message: 'success',
                 data: order
             })
         } catch (e) {
-            onclose.log('e', e)
             reject(e)
         }
     })
